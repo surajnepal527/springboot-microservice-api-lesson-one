7df64ecc0bf04258813178d77c3d81f936f0addb
Suraj <surajnepal34@gmail.com>
2024-03-19 18:38:06 -0600

    Project initialization

---
+++ src/main/java/com/everest/apilessonone/ApiLessonOneApplication.java
@@ -0,0 +1,13 @@
+package com.everest.apilessonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLessonOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLessonOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everest/apilessonone/ApiLessonOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everest.apilessonone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLessonOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
